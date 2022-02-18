266e5e550ea28e4b4aada9e047f13c89389c9b39
Kevin Colonjard <kevincj@outlook.fr>
2022-02-18 09:48:08 +0100

    🔒️ improve error handling: safer error messages

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -10,34 +10,44 @@ import { createToken } from '../utils/token'
 const prisma = new PrismaClient()
 
 export const signup = async (req: Request, res: Response, next: NextFunction) => {
-  const userData: User = req.body
+  const { lastname, firstname, email, password }: User = req.body
 
   try {
-    const hashedPassword = await bcrypt.hash(userData.password, 10)
+    const hashedPassword = await bcrypt.hash(password, 10)
 
     const user = await prisma.user.create({
-      data: { ...userData, password: hashedPassword }
+      data: {
+        lastname,
+        firstname,
+        email,
+        password: hashedPassword
+      }
     })
 
     const accessToken = createToken(user)
 
     res.status(200).json({ user, accessToken })
   } catch (err) {
+    if (err instanceof PrismaClientKnownRequestError) {
+      if (err.code === 'P2002') {
+        next(new createError.BadRequest('Cet utilisateur existe déjà'))
+      }
+    }
     next(err)
   }
 }
 
 export const login = async (req: Request, res: Response, next: NextFunction) => {
-  const userData: User = req.body
+  const { email, password }: User = req.body
 
   try {
     const user = await prisma.user.findUnique({
-      where: { email: userData.email }
+      where: { email }
     })
 
     if (!user) throw new createError.Unauthorized("Cet utilisateur n'existe pas")
 
-    const isValidPassword = await bcrypt.compare(userData.password, user.password)
+    const isValidPassword = await bcrypt.compare(password, user.password)
     if (!isValidPassword) throw new createError.Unauthorized('Le mot de passe est erroné')
 
     const accessToken = createToken(user)
--- src/middleware/handleErrors.ts
+++ src/middleware/handleErrors.ts
@@ -1,10 +1,11 @@
 import { NextFunction, Request, Response } from 'express'
 import { HttpError } from 'http-errors'
 
-export default (err: HttpError, req: Request, res: Response, next: NextFunction) => {
-  const status = err.status || 500
-  const message = err.message || 'Une erreur est survenue'
+export default (err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof HttpError) {
+    return res.status(err.status).json(err.message)
+  }
 
-  res.status(status).send(message)
+  res.status(500).json('Une erreur est survenue')
   next()
 }
