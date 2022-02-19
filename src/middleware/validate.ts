f5f2564a831b27ce16edf43b426af5d24f6b811d
Kevin Colonjard <kevincj@outlook.fr>
2022-02-19 18:38:17 +0100

    🔒️ improve auth validation rules

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -1,13 +1,13 @@
 import { NextFunction, Request, Response } from 'express'
 import createError from 'http-errors'
-import ObjectSchema from 'yup/lib/object'
+import { ObjectSchema, ValidationError } from 'yup'
 
 export default (schema: ObjectSchema<any>) => async (req: Request, res: Response, next: NextFunction) => {
   try {
     await schema.validate(req.body)
     next()
   } catch (err) {
-    if (err instanceof Error) {
+    if (err instanceof ValidationError) {
       next(new createError.BadRequest(err.message))
     }
   }
--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -2,18 +2,19 @@ import * as yup from 'yup'
 
 yup.setLocale({
   mixed: {
-    required: 'Le champ ${path} est requis'
+    required: ({ path }) => `Le champ '${path}' est requis`
   },
   string: {
-    min: 'Minimum ${min} caractères',
-    email: 'Email non valide'
+    min: ({ path, min }) => `Le champ '${path}' doit faire minimum ${min} caractères`,
+    max: ({ path, max }) => `Le champ '${path}' doit faire maximum ${max} caractères`,
+    email: ({ path }) => `Le champ '${path}' doit être un email valide`
   }
 })
 
 export const signupSchema = yup.object({
-  lastname: yup.string().required(),
-  firstname: yup.string().required(),
-  password: yup.string().min(8).required(),
+  lastname: yup.string().trim().min(2).max(50).required(),
+  firstname: yup.string().trim().min(2).max(50).required(),
+  password: yup.string().min(8).max(25).required(),
   email: yup.string().email().required()
 })
 
